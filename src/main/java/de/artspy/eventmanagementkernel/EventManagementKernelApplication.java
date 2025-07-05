ed3da3690235125a53f17970c780f6186f76a8b1
Artur Spychala <artur.spychala@adesso.de>
2025-07-05 18:42:25 +0200

    Initial commit

---
+++ src/main/java/de/artspy/eventmanagementkernel/EventManagementKernelApplication.java
@@ -0,0 +1,13 @@
+package de.artspy.eventmanagementkernel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementKernelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagementKernelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/artspy/eventmanagementkernel/EventManagementKernelApplicationTests.java
@@ -0,0 +1,13 @@
+package de.artspy.eventmanagementkernel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementKernelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
